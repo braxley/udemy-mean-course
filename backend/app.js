e6a016d30c1c859384a881aecb4cb00417dc24b8
braxley <stefan.fathi@gmail.com>
2021-12-07 13:04:14 +0100

    51 added first working requests

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,41 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/api/posts", (req, res, next) => {
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, Content-Type, Accept, X-Requested-With"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTION"
+  );
+  next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({ message: "Successfully posted!" });
+});
+
+app.get("/api/posts", (req, res, next) => {
   const posts = [
-    { title: "First post title", content: "First post content" },
-    { title: "First post title", content: "First post content" },
+    {
+      id: "1298u123hou",
+      title: "First post title",
+      content: "First post content",
+    },
+    {
+      id: "98z12q408u",
+      title: "First post title",
+      content: "First post content",
+    },
   ];
   res.status(200).json({ message: "Fetching successful!", posts: posts });
 });
